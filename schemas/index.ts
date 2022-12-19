e80eed912729047a4126186cd4fcd62d565afa1d
RITIK MAKHIJA <ritikmakhija2002@gmail.com>
2022-12-19 18:30:55 +0530

    Helphub Database

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,127 @@
-export const schemaTypes = []
+export const schemaTypes = [
+    {
+        name: "user",
+        title: "user",
+        type: "document",
+        fields: [
+            {
+                name: "username",
+                title: "Username",
+                type: "string",
+            },
+            {
+                name: "AadharNumber",
+                title: "Aadhar Number",
+                type: "string",
+            },
+            {
+                name: "Email",
+                title: "Email",
+                type: "string",
+            },
+            {
+                name: "MobileNumber",
+                title: "Mobile Number",
+                type: "string",
+            },
+            {
+                name: "Address",
+                title: "Address",
+                type: "string",
+            },
+            {
+                name: "Password",
+                title: "Password",
+                type: "string",
+            },
+        ],
+    },
+    {
+        name: "complaint",
+        title: "complaint",
+        type: "document",
+        fields: [
+            {
+                name: "typeofcomplain",
+                title: "typeofcomplain",
+                type: "string",
+            },
+            {
+                name: "fulladdress",
+                title: "Full Address",
+                type: "string",
+            },
+            {
+                name: "MobileNumber",
+                title: "Mobile Number",
+                type: "string",
+            },
+            {
+                name: "AadharNumber",
+                title: "AadharNumber",
+                type: "string",
+            },
+            {
+                name: "DetailsAboutComplaint",
+                title: "DetailsAboutComplaint",
+                type: "string",
+            },
+            {
+                name: 'user',
+                type: 'object',
+                fields: [
+                    {
+                        title: 'user',
+                        name: 'user',
+                        type: 'reference',
+                        to: [{ type: 'user' }]
+                    }
+                ]
+            }
+        ],
+    },
+    {
+        name: "request",
+        title: "request",
+        type: "document",
+        fields: [
+            {
+                name: "typeofrequest",
+                title: "typeofrequest",
+                type: "string",
+            },
+            {
+                name: "fulladdress",
+                title: "Full Address",
+                type: "string",
+            },
+            {
+                name: "MobileNumber",
+                title: "Mobile Number",
+                type: "string",
+            },
+            {
+                name: "AadharNumber",
+                title: "AadharNumber",
+                type: "string",
+            },
+            {
+                name: "DetailsAboutComplaint",
+                title: "DetailsAboutComplaint",
+                type: "string",
+            },
+            {
+                name: 'user',
+                type: 'object',
+                fields: [
+                    {
+                        title: 'user',
+                        name: 'user',
+                        type: 'reference',
+                        to: [{ type: 'user' }]
+                    }
+                ]
+            },
+        ],
+    },
+]
